2295631552bd1824016fbf292e2970662c1aa453
xiaofan <zeng255@163.com>
2016-05-12 20:11:06 +0800

    3.3.4 TableLayout 允许我们使用表格的方式来排列控件

--- uilayouttest(3.3)/src/main/java/com/zxiaofan/uilayouttest33/MainActivity.java
+++ uilayouttest(3.3)/src/main/java/com/zxiaofan/uilayouttest33/MainActivity.java
@@ -29,6 +29,14 @@ public class MainActivity extends AppCompatActivity  {
                 startActivity(intent);
             }
         });
+        Button button2= (Button) findViewById(R.id.button_TableLayout);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,TableLayout.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ uilayouttest(3.3)/src/main/java/com/zxiaofan/uilayouttest33/TableLayout.java
@@ -0,0 +1,15 @@
+package com.zxiaofan.uilayouttest33;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yunhai on 2016/5/12.
+ */
+public class TableLayout extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tableayout);
+    }
+}
