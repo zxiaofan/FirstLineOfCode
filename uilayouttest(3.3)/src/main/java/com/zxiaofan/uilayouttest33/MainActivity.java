48c7064ef3aa0cd1a0d53d517646b0ece39a86be
zxiaofan <zxiaofan@users.noreply.github.com>
2016-05-12 15:06:17 +0800

    3.3.2 RelativeLayout 又称作相对布局.通过相对定位的方式让控件出现在布局的任何位置

--- uilayouttest(3.3)/src/main/java/com/zxiaofan/uilayouttest33/MainActivity.java
+++ uilayouttest(3.3)/src/main/java/com/zxiaofan/uilayouttest33/MainActivity.java
@@ -1,13 +1,35 @@
 package com.zxiaofan.uilayouttest33;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relativelayout);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_RelativeLayout:
+                button = (Button) findViewById(R.id.button_RelativeLayout);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, RelativeLayout.class);
+                        startActivity(intent);
+                    }
+                });
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ uilayouttest(3.3)/src/main/java/com/zxiaofan/uilayouttest33/RelativeLayout.java
@@ -0,0 +1,16 @@
+package com.zxiaofan.uilayouttest33;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yunhai on 2016/5/12.
+ */
+public class RelativeLayout extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
